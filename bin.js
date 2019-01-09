073ac1dea93ba69127e9b0f8ec6ec2fd302e7282
Seth Bolander <bol15015@byui.edu>
2019-01-09 16:05:39 -0700

    added csv functionality

--- bin.js
+++ bin.js
@@ -1,27 +1,35 @@
 const main = require('./main.js');
 const inquirer = require('inquirer');
 const d3 = require('d3-dsv');
+const fs = require('fs');
 
 const questions = [{
-    type: 'list',
-    name: 'csvormanual',
-    message: 'CSV or Manual?',
-    choices: ['CSV', 'Manual']
-}, {
-    type: 'input',
-    name: 'filepath',
-    message: 'Please enter the CSV filepath:',
-    validate: answer => /.csv$/.test(answer),
-    when: answers => answers.csvormanual === 'CSV'
-},
-{
-    type: 'input',
-    name: 'courseID',
-    message: 'Please enter the course ID:',
-    validate: answer => /^[0-9]+$/.test(answer),
-    when: answers => answers.csvormanual === 'Manual'
-}
+        type: 'list',
+        name: 'csvormanual',
+        message: 'CSV or Manual?',
+        choices: ['CSV', 'Manual']
+    }, {
+        type: 'input',
+        name: 'filepath',
+        message: 'Please enter the CSV filepath:',
+        validate: answer => /.csv$/.test(answer),
+        when: answers => answers.csvormanual === 'CSV'
+    },
+    {
+        type: 'input',
+        name: 'courseID',
+        message: 'Please enter the course ID:',
+        validate: answer => /^[0-9]+$/.test(answer),
+        when: answers => answers.csvormanual === 'Manual'
+    }
 ];
+
+function parseCSV(path) {
+    var csvData = fs.readFileSync(path, 'utf8');
+    let parsedCSV = d3.csvParse(csvData);
+
+    return parsedCSV.map(courseID => courseID['Course ID']);
+}
 
 function getInput(callback) {
     var inputObj;
@@ -29,8 +37,15 @@ function getInput(callback) {
         .then(answers => {
             if (answers.csvormanual === 'CSV') {
                 // Parse CSV and create Object
+                var courseIDs = parseCSV(answers.filepath);
+                inputObj = {
+                    courseIDs: courseIDs
+                };
             } else {
                 // Create object
+                inputObj = {
+                    courseIDs: [answers.courseID]
+                };
             }
             callback(inputObj, getOutput);
         }, console.error);

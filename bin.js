8c0ce63da6e9245d18ba23074448b0fbb8d22bd1
Wilson <kilakal@byui.edu>
2019-01-09 17:39:27 -0700

    Got quiz questions

--- bin.js
+++ bin.js
@@ -4,24 +4,24 @@ const d3 = require('d3-dsv');
 const fs = require('fs');
 
 const questions = [{
-        type: 'list',
-        name: 'csvormanual',
-        message: 'CSV or Manual?',
-        choices: ['CSV', 'Manual']
-    }, {
-        type: 'input',
-        name: 'filepath',
-        message: 'Please enter the CSV filepath:',
-        validate: answer => /.csv$/.test(answer),
-        when: answers => answers.csvormanual === 'CSV'
-    },
-    {
-        type: 'input',
-        name: 'courseID',
-        message: 'Please enter the course ID:',
-        validate: answer => /^[0-9]+$/.test(answer),
-        when: answers => answers.csvormanual === 'Manual'
-    }
+    type: 'list',
+    name: 'csvormanual',
+    message: 'CSV or Manual?',
+    choices: ['CSV', 'Manual']
+}, {
+    type: 'input',
+    name: 'filepath',
+    message: 'Please enter the CSV filepath:',
+    validate: answer => /.csv$/.test(answer),
+    when: answers => answers.csvormanual === 'CSV'
+},
+{
+    type: 'input',
+    name: 'courseID',
+    message: 'Please enter the course ID:',
+    validate: answer => /^[0-9]+$/.test(answer),
+    when: answers => answers.csvormanual === 'Manual'
+}
 ];
 
 function parseCSV(path) {
@@ -38,14 +38,10 @@ function getInput(callback) {
             if (answers.csvormanual === 'CSV') {
                 // Parse CSV and create Object
                 var courseIDs = parseCSV(answers.filepath);
-                inputObj = {
-                    courseIDs: courseIDs
-                };
+                inputObj = courseIDs;
             } else {
                 // Create object
-                inputObj = {
-                    courseIDs: [answers.courseID]
-                };
+                inputObj = [answers.courseID];
             }
             callback(inputObj, getOutput);
         }, console.error);
--- main.js
+++ main.js
@@ -1,9 +1,56 @@
 /*************************************************************************
  * Module Description
  *************************************************************************/
-function main(data, callback) {
-    console.log(data);
-    return callback(null, data);
+const asyncLib = require('async');
+const canvas = require('canvas-api-wrapper');
+
+function getQuizQuestions(quizObj, callback) {
+    canvas.getQuizQuestions(quizObj.courseID, quizObj.id, (err, questions) => {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        callback(null, questions);
+    });
+}
+
+function getQuizzes(courseID, callback) {
+    canvas.getQuizzes(courseID, (err, quizzes) => {
+        if (err) {
+            console.error(err);
+            callback(err, null);
+            return;
+        }
+        quizzes = quizzes.map(quiz => {
+            return {
+                id: quiz.id,
+                courseID
+            };
+        });
+        asyncLib.mapLimit(quizzes, 10, getQuizQuestions, (err, data) => {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, data);
+        });
+    });
+}
+
+function main(courseIDs, callback) {
+    asyncLib.mapLimit(courseIDs, 10, getQuizzes, (err, data) => {
+        data = data[0][24];
+        if (err) {
+            console.error(err);
+            callback(err, null);
+            return;
+        }
+        console.dir(data, {
+            depth: null
+        });
+        console.log(data.length);
+        callback(null, data);
+    });
 }
 
 module.exports = main;
